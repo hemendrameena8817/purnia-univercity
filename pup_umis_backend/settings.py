08eb3926b929a603fb800c58a29e5fa7b26a1a46
dev-shubhh <shubhamzucol@gmail.com>
2026-01-15 18:20:32 +0530

    feat: initialize Django project with JWT auth, CORS, and basic URL routing

--- pup_umis_backend/settings.py
+++ pup_umis_backend/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'                       # existing
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Default primary key field type
@@ -203,3 +204,5 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
