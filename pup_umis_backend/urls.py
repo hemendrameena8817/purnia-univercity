36466d7e36ac36b9a863bdf44dea5e57ec516de6
dev-shubhh <shubhamzucol@gmail.com>
2026-01-15 18:25:08 +0530

    feat: add initial URL routing for admin, accounts and students endpoints

--- pup_umis_backend/urls.py
+++ pup_umis_backend/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('students/', include('students.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/students/', include('students.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
