7d8ae567366b25f33b6126b5296df7ffc0ecc619
shankarzucol <shankar.kumar@zucolservices.com>
2026-01-15 13:29:52 +0530

    university:

--- university/admin.py
+++ university/admin.py
@@ -1,18 +1,35 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import University
 
 
 @admin.register(University)
 class UniversityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'short_name', 'vice_chancellor', 'email', 'contact_no', 'established_date')
-    search_fields = ('name', 'short_name', 'email')
+    list_display = (
+        'name',
+        'short_name',
+        'logo_preview',
+        'vice_chancellor',
+        'email',
+        'contact_no',
+        'established_date',
+    )
+    
+    search_fields = (
+        'name',
+        'short_name',
+        'email',
+        'vice_chancellor',
+    )
+    
     list_filter = ('established_date',)
     ordering = ('name',)
-    readonly_fields = ('uid', 'created_at', 'updated_at')
     
+    readonly_fields = ('uid', 'created_at', 'updated_at', 'logo_preview')
+
     fieldsets = (
         ('Basic Information', {
-            'fields': ('uid', 'name', 'short_name', 'logo')
+            'fields': ('uid', 'name', 'short_name', 'logo', 'logo_preview')
         }),
         ('Contact Information', {
             'fields': ('address', 'contact_no', 'email', 'website')
@@ -29,3 +46,20 @@ class UniversityAdmin(admin.ModelAdmin):
             'classes': ('collapse',)
         }),
     )
+
+    def logo_preview(self, obj):
+        """
+        Display a small preview of the university logo in the admin panel.
+        """
+        if obj.logo:
+            return format_html('<img src="{}" width="60" height="60" style="object-fit:contain;" />', obj.logo.url)
+        return "No Logo"
+    
+    logo_preview.short_description = "Logo Preview"
+
+    def has_add_permission(self, request):
+        """
+        Allow only one University record in the system.
+        Remove this method if you want to allow multiple universities.
+        """
+        return not University.objects.exists()
--- university/models.py
+++ university/models.py
@@ -6,17 +6,17 @@ class University(models.Model):
     """
     Represents a University. In this system, all colleges belong to Purnea University.
     """
-    uid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
-    name = models.CharField(max_length=255)
-    short_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255, unique=True)
+    short_name = models.CharField(max_length=100, unique=True)
     logo = models.ImageField(upload_to='university_logos/', null=True, blank=True)
     address = models.TextField()
     vice_chancellor = models.CharField(max_length=255)
-    contact_no = models.CharField(max_length=15)
-    email = models.EmailField()
+    contact_no = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(unique=True)
     established_date = models.DateField()
-    website = models.URLField()
+    website = models.URLField(unique=True, blank=True)
     json_data = models.JSONField(null=True, blank=True)
     
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,6 +25,10 @@ class University(models.Model):
     class Meta:
         verbose_name = 'University'
         verbose_name_plural = 'Universities'
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['short_name']),
+        ]
 
     def __str__(self):
         return self.name
