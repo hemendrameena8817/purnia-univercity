29fa875d5a7b9f7bbf8655ee3a6a071e37b020a2
anuprash <anuprash.gupta@zucolservices.com>
2026-01-23 00:23:23 +0530

    add models for UG coursestructure and student course structure

--- academics/models.py
+++ academics/models.py
@@ -495,3 +495,135 @@ class Professor(models.Model):
             return self.user.get_full_name()
         return f"{self.first_name} {self.last_name}"
 
+
+class CourseStructure(models.Model):
+    """
+    Represents the course structure for a program.
+    """
+    uid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    name = models.CharField(max_length=100, null=True, blank=True, help_text="Course Name")
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE,
+        related_name='course_structures'
+    )
+    course_type = models.CharField(max_length=20, null=True, blank=True, help_text="Course Type")
+    code = models.CharField(max_length=20, null=True, blank=True, help_text="Course Code")
+    max_credit = models.IntegerField(null=True, blank=True, help_text="Course Credit")
+    max_marks = models.IntegerField(null=True, blank=True, help_text="Course Marks")
+
+    min_mark = models.IntegerField(null=True, blank=True, help_text="Pass Mark")
+    min_credit = models.IntegerField(null=True, blank=True, help_text="Min Credit")
+
+    description = models.TextField(null=True, blank=True, help_text="Course Description")
+    label = models.CharField(max_length=100,help_text="Assessment label (e.g. CIA-Theory, ESE-Practical)")
+   
+    semester = models.IntegerField(null=True, blank=True, help_text="Semester")
+    json_data = models.JSONField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Course Structure'
+        verbose_name_plural = 'Course Structures'
+
+    def __str__(self):
+        return f"{self.department.name} - {self.course_type}"
+
+
+class StudentCourseAssessment(models.Model):
+    """
+    Semester-wise assessment + marks for a student course
+    using flexible labels (CIA-Theory, ESE-Practical, etc.)
+    """
+    uid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    student = models.ForeignKey(
+        'students.Student',
+        on_delete=models.CASCADE,
+        related_name='course_structures'
+    )
+    course_type = models.CharField(max_length=20, null=True, blank=True, help_text="Course Type")
+    code = models.CharField(max_length=20, null=True, blank=True, help_text="Course Code")
+    semester = models.IntegerField(null=True, blank=True, help_text="Semester")
+
+    max_credit = models.IntegerField(null=True, blank=True, help_text="Course Credit")
+    max_marks = models.IntegerField(null=True, blank=True, help_text="Course Marks")
+
+    min_mark = models.IntegerField(null=True, blank=True, help_text="Pass Mark")
+    min_credit = models.IntegerField(null=True, blank=True, help_text="Min Credit")
+
+    description = models.TextField(null=True, blank=True, help_text="Course Description")
+    label = models.CharField(max_length=100,help_text="Assessment label (e.g. CIA-Theory, ESE-Practical)")
+
+    marks_obtained = models.IntegerField(null=True, blank=True, help_text="Marks Obtained")
+    credit_obtained = models.IntegerField(null=True, blank=True, help_text="Credit Obtained")
+
+    grade = models.CharField(max_length=10, null=True, blank=True, help_text="Grade")
+    numeric_grade = models.IntegerField(null=True, blank=True, help_text="Numeric Grade")
+
+    is_absent = models.BooleanField(default=False, help_text="Is Absent")
+
+    status = models.CharField(max_length=10, null=True, blank=True, help_text="Status pass/fail/promoted")
+    json_data = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Student Course Assessment'
+        verbose_name_plural = 'Student Course Assessments'
+
+    def __str__(self):
+        return f"{self.student_course} | Sem {self.semester} | {self.label}"
+
+
+class SemesterRegistration(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    student = models.ForeignKey(
+        'students.Student',
+        on_delete=models.CASCADE,
+        related_name='semester_registrations'
+    )
+    start_date = models.DateTimeField(null=True, blank=True, help_text="Start Date")
+    end_date = models.DateTimeField(null=True, blank=True, help_text="End Date")
+    is_open = models.BooleanField(default=False, help_text="Is Open")
+    sem = models.IntegerField(null=True, blank=True, help_text="Semester")
+    status = models.CharField(max_length=10, null=True, blank=True, help_text="Status open/closed")
+    exam_eligible = models.BooleanField(default=False, help_text="Eligible for Exam")
+    remarks = models.TextField(null=True, blank=True, help_text="Remarks")
+    next_sem_eligible = models.BooleanField(default=False, help_text="Eligible for Next Semester")
+    json_data = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Semester Registration'
+        verbose_name_plural = 'Semester Registrations'
+
+    def __str__(self):
+        return f"{self.student.name}"
+
+
+class ExamRegistration(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    student = models.ForeignKey(
+        'students.Student',
+        on_delete=models.CASCADE,
+        related_name='exam_registrations'
+    )
+    start_date = models.DateTimeField(null=True, blank=True, help_text="Start Date")
+    end_date = models.DateTimeField(null=True, blank=True, help_text="End Date")
+    is_open = models.BooleanField(default=False, help_text="Is Open")
+    fees = models.IntegerField(null=True, blank=True, help_text="Fees")
+    sem = models.IntegerField(null=True, blank=True, help_text="Semester")
+    status = models.CharField(max_length=10, null=True, blank=True, help_text="Status")
+    json_data = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Exam Registration'
+        verbose_name_plural = 'Exam Registrations'
+
+    def __str__(self):
+        return f"{self.student.name}"
