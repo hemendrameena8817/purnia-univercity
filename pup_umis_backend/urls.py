cebaf1d9f3728d8c17db90b7ddb6c01508ada80f
dev-shubhh <shubhamzucol@gmail.com>
2026-02-08 21:05:24 +0530

    changed admin url

--- pup_umis_backend/urls.py
+++ pup_umis_backend/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("super-admin-umis/", admin.site.urls),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
