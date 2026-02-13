b9948c691810753f18cdeaf6345c518e6cf7cfad
gauravj-github <gauravjharbade2003@gmail.com>
2026-02-13 13:27:04 +0530

    change on pg get fill marks view

--- pg/views.py
+++ pg/views.py
@@ -186,6 +186,7 @@ class PGCollegeStudentsView(APIView):
                 'ind_max_marks': assessment.ind_max_marks,
                 'ind_pass_marks': assessment.ind_pass_marks,
                 'ind_marks_obtained': assessment.ind_marks_obtained,
+                'ind_is_absent': assessment.ind_is_absent,
                 'is_cia_fill': assessment.is_cia_fill,
                 'updated_at': assessment.updated_at.strftime('%d-%m-%Y %I:%M %p') if assessment.updated_at else None
             })
