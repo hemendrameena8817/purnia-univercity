85ea0635a6a7b33fc1742d615e6209d324f3a68a
Anuprash-4321 <113698240+Anuprash-4321@users.noreply.github.com>
2026-02-17 16:22:09 +0530

    fix

--- ug/utils/registration_card_pdf.py
+++ ug/utils/registration_card_pdf.py
@@ -198,7 +198,7 @@ def generate_registration_card(student, registration, assessments):
         'college_name': student.user.college.name if student.user.college else "",
         'course_name': student.program.name.split('-')[0].strip() if student.program else "-",
         'semester_text': sem_text,
-        'session': f"({registration.batch.name if registration.batch else '2024-28'})".strip(),
+        'session': registration.batch.name,
         
         'major_subject_name': major_subject_name,
         'minor_subject_name': minor_subject_name,
